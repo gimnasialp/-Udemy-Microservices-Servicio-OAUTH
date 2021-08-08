a97e2aeffae371c9cbee9fe1a3b06ab6fb6ef8b9
migue_centellas_leon <migue.centellas@gmail.com>
2021-08-08 20:21:43 -0300

    76. Creando nuestra interfaz Feign Client

---
+++ src/main/java/com/formaionbdi/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,14 @@
+package com.formaionbdi.springboot.app.oauth.clients;
+
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/search/buscar-username")
+    public Usuario findByUsername(@RequestParam String username);
+
+}
